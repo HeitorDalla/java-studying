692bad914b1d423548e9ae635e36fc3a16a8ea74
HeitorDalla <heitorvillavilla@gmail.com>
2025-08-07 10:19:54 -0300

    Adicionando atividade 2

---
+++ Atividade2/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main (String[] args) {
+        System.out.println("Ola a todos");
+
+        // Criando variáveis
+        int idade;
+        boolean verdadeiro;
+        char letra;
+        double numeroQuebrado;
+        float outroNumero;
+
+        // Para a String, tem que ser '' aspas simples
+        letra = 'A';
+        verdadeiro = true;
+        verdadeiro = false;
+        numeroQuebrado = 3.14;
+        outroNumero = 3.14f;
+
+        // Imprimindo as variáveis
+        System.out.println("Voce digitou a letra " + letra);
+    }
+}
