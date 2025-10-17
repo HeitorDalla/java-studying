cd9a14871844448ffe089d198aeab9fc04ab50cf
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-17 19:13:40 -0300

    Finalizando a classe principal e result

--- Aplicações/CalculadoraAvancada/src/App.java
+++ Aplicações/CalculadoraAvancada/src/App.java
@@ -1,8 +1,69 @@
 import java.util.Scanner;
 
+import br.edu.unicesumar.classes.Calculator;
+import br.edu.unicesumar.classes.Result;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("--- Opções ---");
+            System.out.println("--- 1 - Somar ---");
+            System.out.println("--- 2 - Subtrair ---");
+            System.out.println("--- 3 - Multiplicar ---");
+            System.out.println("--- 4 - Dividir ---");
+            System.out.println("--- 0 - Sair ---");
+
+            System.out.println("Qual opção voce deseja? ");
+            int opcao = sc.nextInt();
+
+            if (opcao == 0) break;
+
+            System.out.println("Digite o primeiro numero: ");
+            double num1 = sc.nextDouble();
+
+            System.out.println("Digite o segundo numero: ");
+            double num2 = sc.nextDouble();
+
+            double resultado = 0;
+
+            switch (opcao) {
+                case 1:
+                    resultado = Calculator.add(num1, num2);
+                    break;
+
+                case 2:
+                    resultado = Calculator.sub(num1, num2);
+                    break;
+
+                case 3:
+                    resultado = Calculator.multi(num1, num2);
+                    break;
+
+                case 4:
+                    resultado = Calculator.divi(num1, num2);
+                    break;
+            
+                default:
+                    System.out.println("Não existe essa opção!");
+                    break;
+            }
+
+            Result r = new Result(resultado);
+            System.out.println("O resultado da operação é: " + r.getValue());
+
+            System.out.println("Voce deseja continuar? [S/N] ");
+            sc.nextLine();
+            String pergunta = sc.nextLine();
+
+
+            if (pergunta.equalsIgnoreCase("n")) {
+                System.out.println("Programa encerrando... ");
+                break;
+            }
+        }
+
+        sc.close();
     }
 }
--- Aplicações/CalculadoraAvancada/src/br/edu/unicesumar/classes/Result.java
+++ Aplicações/CalculadoraAvancada/src/br/edu/unicesumar/classes/Result.java
@@ -1,5 +1,13 @@
 package br.edu.unicesumar.classes;
 
 public class Result {
-    
+    private final double value;
+
+    public Result (double value) {
+        this.value = value;
+    }
+
+    public double getValue () {
+        return this.value;
+    }
 }
