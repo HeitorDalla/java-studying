3bfd0bb8dfe5bcc57efae9394b26e6cf9f94e7c5
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-18 10:01:18 -0300

    Finalizando a classe para armazenar o estado da professora

--- Aplicações/SistemaEscolar/src/br/edu/unicesumar/classes/Professora.java
+++ Aplicações/SistemaEscolar/src/br/edu/unicesumar/classes/Professora.java
@@ -1,5 +1,25 @@
 package br.edu.unicesumar.classes;
 
 public class Professora extends Pessoa {
-    
+    private String cargaHoraria;
+
+    public Professora () {}
+
+    public Professora (String nome, int idade, String cargaHoraria) {
+        super(nome, idade);
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getCargaHoraria () {
+        return this.cargaHoraria;
+    }
+
+    public void setCargaHoraria (String cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String showInfo () {
+        return "Nome: " + getNome() + ", Idade: " + getIdade() + ", Carga horaria: " + getCargaHoraria();
+    }
 }
