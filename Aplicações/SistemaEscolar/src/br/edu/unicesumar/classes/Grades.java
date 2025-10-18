95186c488aefedc216cf9871ea147a24a6a7b17b
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-18 10:00:56 -0300

    Finalizado a classe de armazenamento de notas

---
+++ Aplicações/SistemaEscolar/src/br/edu/unicesumar/classes/Grades.java
@@ -0,0 +1,42 @@
+package br.edu.unicesumar.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grades {
+    private List<Double> gradesList;
+
+    public Grades () {}
+
+    public Grades (int numeroNotas) {
+        // Cria uma nova instancia vazia quando o objeto Grades é instanciado
+        this.gradesList = new ArrayList<>();
+    }
+
+    // retornar a lista completa de notas
+    public List<Double> getGradesList () {
+        return this.gradesList;
+    }
+
+    // adicionar notas à lista
+    public void addGrade (double grade) { //receber a entrada da nota
+        this.gradesList.add(grade);
+    }
+
+    // calcular a média das notas
+    public double calculateAverage () { // nao precisa receber nada, pois ele pega do estado da variavel
+        // Verificar se ele esta vazio
+        if (this.gradesList.isEmpty()) {
+            return 0.0;
+        }
+        
+        // soma todas as notas
+        double somaNotas = 0;
+        for (double nota : gradesList) {
+            somaNotas += nota;
+        }
+
+        // retorna a média
+        return somaNotas / this.gradesList.size();
+    }
+}
